3d8b7d71c7f953c85f0df14bf2089a2d75aa1284
Matt Wilkes <matthew.wilkes90@gmail.com>
2024-08-28 20:49:29 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Matt Wilkes on 28/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
